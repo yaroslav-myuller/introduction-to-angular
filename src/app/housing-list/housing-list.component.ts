83fca20e01d97644e85dbd2f39d603024b4f0165
Yaroslav Myuller <yaroslav.myuller@ista.com>
2023-01-31 13:35:06 +0100

    add housing list component and first template implementation

---
+++ src/app/housing-list/housing-list.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-housing-list',
+  templateUrl: './housing-list.component.html',
+  styleUrls: ['./housing-list.component.css']
+})
+export class HousingListComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  searchHousingLocations(searchText: string) {
+    console.log(searchText);
+  }
+}
